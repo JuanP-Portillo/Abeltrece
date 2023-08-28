61a26a343591267f81b24d0c332b3152a8d98842
Juan Pablo Portillo <juanp.portillob@gmail.com>
2023-08-28 12:17:14 -0600

    nearly done

---
+++ app/models/contact_submission.rb
@@ -0,0 +1,7 @@
+class ContactSubmission < ApplicationRecord
+  #validates :name, presence: true
+  #validates :email, presence: true
+  #validates :phone, presence: true
+  #validates :message, presence: true
+
+end
